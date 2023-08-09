b307d1da57a4120afed9db5d03b55992c289e1a6
concept10 <tenthconcept@gmail.com>
2023-08-08 18:18:17 -0700

    add extension.js

---
+++ extension.js
@@ -0,0 +1,12 @@
+// extension.js
+//
+
+const ExtensionUtils = imports.misc.exstensionUtils.js;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const exstensioDirectory = Me.dir.get_path();
+
+
+console.log(extensionDirectory);
+
+
